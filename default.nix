622d26680c38e005f72896fee15a534e6db021aa
Jamie Brandon <jamie@scattered-thoughts.net>
2019-11-05 21:04:01 +0000

    Update hash

--- default.nix
+++ default.nix
@@ -6,8 +6,8 @@ pkgs.rustPlatform.buildRustPackage {
   src = pkgs.fetchFromGitHub {
     owner = "jamii";
     repo = "rust-nix-patch";
-    rev = "4dd0e1e1b6179fefbc6c57f1af9acdb5500ad434";
-    sha256 = "0z4p7yxbr0avkx3yr61l6x4mak15la8j3514ldjgsx9bhfmznksj";
+    rev = "0a99ce4a56d5e9ef405b6bd10a10812b1b791149";
+    sha256 = "1gl8jhs1kym27qfg1fmxrdzhvmxii42fm6cwy50j8psf79razvya";
   };
 
   cargoSha256 = "1563mwq6xmfkn05h4i0bfdc6laydcc9s89y0x1xw5974byv2ss9y";
